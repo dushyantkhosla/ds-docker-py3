0ffcde6df472da465e90eec17a3f1195dd887b13
Dushyant Khosla <dushyant.khosla@pmi.com>
2018-03-01 16:15:32 +0100

    Edited start.sh

--- start.sh
+++ start.sh
@@ -3,14 +3,13 @@ source activate ds-py3
 
 git config --global user.name $GIT_USER_NAME
 git config --global user.email $GIT_USER_MAIL
+git config --global http.proxy http://squid.service.pmicicd.ocean:3128
 
 # Print information
 echo ""
 echo ""
-echo "To get Fish and OMF, run ..."
-echo "-----------------------------------------------------------------"
-echo "curl -sSL https://oh-my.fish | fish"
-echo "omf install agnoster"
+
+echo ""
 echo ""
 echo "To get the Cookiecutter, run ..."
 echo "-----------------------------------------------------------------"
@@ -18,12 +17,18 @@ echo "git clone https://github.com/dushyantkhosla/ds-template-01.git"
 echo ""
 echo "To start a Jupyter notebook server, run ..."
 echo "-----------------------------------------------------------------"
-echo "jupyter notebook --allow-root --no-browser --ip 0.0.0.0 --port 8080"
+echo "jupyter notebook --script --allow-root --no-browser --ip 0.0.0.0 --port 8080"
 echo ""
 echo "Configure git. Run ..."
 echo "-----------------------------------------------------------------"
 echo "git config --global user.email 'you@example.com'"
 echo "git config --global user.name 'Your Name'"
+echo ""
+echo "If you're running this container inside the ocean, run this to access git repo's"
+echo "---------------------------------------------------------------------------------"
+echo "git config --global http.proxy http://squid.service.pmicicd.ocean:3128"
+echo ""
+echo ""
 
 echo ""
 echo ""
